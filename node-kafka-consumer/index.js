80cb1137f5780e9254e2af6a23703a160ab09f75
Mark Disbrow <disbrm1@nationwide.com>
2019-10-29 09:39:45 -0400

    EXPOSE 9092 on docker broker

--- node-kafka-consumer/index.js
+++ node-kafka-consumer/index.js
@@ -1,20 +1,25 @@
 let kafka = require('kafka-node');
-let client = new kafka.KafkaClient({
-                    kafkaHost: 'localhost:9092'
-                 });
 
+try {
+    let client = new kafka.KafkaClient({
+        kafkaHost: 'localhost:92'
+                     });
 
-let consumer = new kafka.Consumer(client,
-                                  [{topic: 'TestTopic'}]);
+    let consumer = new kafka.Consumer(client,
+                                  [{topic: 'TestTopic', partition: 0}]);
 
-console.log('Beginning to listen to topic `testTopic`');
+    console.log('Attempting to listen to topic `TestTopic`');
 
-consumer.on('error', (err) => {
-    console.log('There was an error:');
-    console.log(err);
-});
+    consumer.on('error', (err) => {
+        console.log('There was an error:');
+        console.log(err);
+    });
 
-consumer.on('message', (message) => {
-    console.log('New message incoming...');
-    console.log(message);
-});
+    consumer.on('message', (message) => {
+        console.log('New message incoming...');
+        console.log(message);
+    });
+} catch (e) {
+    console.log('Error...');
+    console.log(e);
+}
